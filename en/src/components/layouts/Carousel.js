756479232aba56ade21187cedc73fb17ea467ac8
guilhermescr <guilhermechaves2005@hotmail.com>
2022-10-11 17:35:22 -0300

    adjusted carousel system

--- en/src/components/layouts/Carousel.js
+++ en/src/components/layouts/Carousel.js
@@ -3,7 +3,7 @@ import styles from './Carousel.module.css';
 import RamMemory from '../../img/carousel_images/ram_memory.webp';
 import RTX_3080 from '../../img/carousel_images/rtx_3080.jpg';
 import Ryzen7_5700G from '../../img/carousel_images/ryzen7_5700g.webp';
-import Keyboard_VideoGame_Controller from '../../img/carousel_images/keyboard_with_a_playstation_controller.jpg';
+import PC_and_Laptop from '../../img/carousel_images/pc_and_laptop.jpg';
 
 function Carousel() {
   setTimeout(() => {
@@ -11,23 +11,77 @@ function Carousel() {
     const CAROUSEL_IMAGES = document.querySelectorAll('#carouselImg img');
     const PREV_BUTTON = document.getElementById('slide-arrow-prev');
     const NEXT_BUTTON = document.getElementById('slide-arrow-next');
-    let currentImageScroll = 0;
+    let currentImage = 0;
+    let imagePercentage;
+    let clickFired = false;
 
     NEXT_BUTTON.addEventListener('click', () => {
-      currentImageScroll++;
+      clickFired = true;
 
-      CAROUSEL_CONTAINER.scrollLeft =
-        CAROUSEL_IMAGES[0].clientWidth * currentImageScroll;
-    });
-    PREV_BUTTON.addEventListener('click', () => {
-      if (currentImageScroll !== 0) {
-        currentImageScroll--;
+      if (clickFired) {
+        currentImage++;
+      }
+
+      if (currentImage === CAROUSEL_IMAGES.length) {
+        currentImage = 0;
       }
 
       CAROUSEL_CONTAINER.scrollLeft =
-        CAROUSEL_IMAGES[0].clientWidth * currentImageScroll;
+        CAROUSEL_IMAGES[0].clientWidth * currentImage;
+
+      setTimeout(() => {
+        clickFired = false;
+      }, 1500);
     });
-  }, 1500);
+
+    PREV_BUTTON.addEventListener('click', () => {
+      console.log(currentImage, CAROUSEL_IMAGES[0].clientWidth * currentImage);
+      clickFired = true;
+
+      if (currentImage === 0) {
+        currentImage = CAROUSEL_IMAGES.length;
+      } else {
+        currentImage--;
+      }
+
+      if (currentImage === CAROUSEL_IMAGES.length) {
+        currentImage--;
+      }
+
+      CAROUSEL_CONTAINER.scrollLeft =
+        CAROUSEL_IMAGES[0].clientWidth * currentImage;
+    });
+
+    CAROUSEL_CONTAINER.addEventListener('scroll', function () {
+      if (clickFired) return;
+
+      imagePercentage = (60 / 100) * CAROUSEL_IMAGES[0].clientWidth;
+
+      for (let index = 0; index < CAROUSEL_IMAGES.length; index++) {
+        if (
+          CAROUSEL_CONTAINER.scrollLeft <
+          CAROUSEL_IMAGES[0].clientWidth / 50
+        ) {
+          currentImage = 0;
+          return;
+        }
+
+        if (
+          CAROUSEL_CONTAINER.scrollLeft > imagePercentage * index &&
+          CAROUSEL_CONTAINER.scrollLeft < CAROUSEL_IMAGES[0].clientWidth * index
+        ) {
+          imagePercentage = (60 / 100) * (CAROUSEL_IMAGES[0].clientWidth * 2);
+
+          currentImage = index;
+        }
+
+        console.log(`CONTAINER SCROLL: ${CAROUSEL_CONTAINER.scrollLeft}
+        IMAGE: ${CAROUSEL_IMAGES[0].clientWidth * (currentImage + 1)}
+        IMAGE PERCENTAGE: ${imagePercentage}
+        CURRENT IMAGE: ${currentImage}`);
+      }
+    });
+  }, 1000);
 
   return (
     <section className={styles.carousel}>
@@ -47,12 +101,16 @@ function Carousel() {
       </button>
 
       <div className={styles.container} id="carouselImg">
-        <img src={RamMemory} alt="Ram Memory Image" />
-        <img src={RTX_3080} alt="GPU RTX 3080 Image" />
-        <img src={Ryzen7_5700G} alt="CPU Ryzen 7 5700G Image" />
         <img
-          src={Keyboard_VideoGame_Controller}
-          alt="A RGB keyboard with a playstation controller and a headphone"
+          src={RamMemory}
+          alt="The Best Ram Memory for 3rd Generation AMD Ryzen CPUs"
+        />
+        <img src={RTX_3080} alt="The Ultimate GPU GeForce RTX 3080" />
+        <img src={Ryzen7_5700G} alt="A box with CPU Ryzen 7 5700G" />
+        <img
+          id={styles.pc_and_laptop}
+          src={PC_and_Laptop}
+          alt="3 laptops, a monitor and a pc cabinet with a blue background"
         />
       </div>
     </section>
